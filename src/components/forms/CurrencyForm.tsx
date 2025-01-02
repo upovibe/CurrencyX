0f3dfcbbbddbd06b0f7339310f835fbd2588a509
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-02 21:14:16 +0000

    refactor: simplify CurrencyForm component by removing unused variants and updating options

--- src/components/forms/CurrencyForm.tsx
+++ src/components/forms/CurrencyForm.tsx
@@ -1,27 +1,25 @@
-import { Input, For } from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react";
 import {
   NativeSelectField,
   NativeSelectRoot,
-} from "@/components/ui/native-select"
+} from "@/components/ui/native-select";
 
 const CurrencyForm = () => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <For each={["outline", "subtle", "plain"]}>
-        {(variant) => (
-          <NativeSelectRoot variant={variant} key={variant}>
-            <NativeSelectField placeholder={`variant (${variant})`}>
-              <option value="react">React</option>
-              <option value="vue">Vue</option>
-              <option value="angular">Angular</option>
-              <option value="svelte">Svelte</option>
-            </NativeSelectField>
-          </NativeSelectRoot>
-        )}
-      </For>
-      <Input placeholder="Subtle" variant="subtle" className="px-3 rounded-" />
+      <NativeSelectRoot className="border-gray-800">
+        <NativeSelectField>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </NativeSelectField>
+      </NativeSelectRoot>
+      <Input
+        placeholder="Subtle"
+        variant="subtle"
+        className="px-3 rounded-lg"
+      />
     </form>
-  )
-}
+  );
+};
 
-export default CurrencyForm
+export default CurrencyForm;
