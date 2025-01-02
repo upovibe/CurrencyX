0f158f3680b54a2a4379f0a1c26fb73e5e5b42f5
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-02 20:29:29 +0000

    feat: implement CurrencyForm component with Chakra UI native select and input fields

--- src/components/forms/CurrencyForm.tsx
+++ src/components/forms/CurrencyForm.tsx
@@ -1,6 +1,26 @@
+import { Input, For } from "@chakra-ui/react"
+import {
+  NativeSelectField,
+  NativeSelectRoot,
+} from "@/components/ui/native-select"
+
 const CurrencyForm = () => {
   return (
-    <div>CurrencyForm</div>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <For each={["outline", "subtle", "plain"]}>
+        {(variant) => (
+          <NativeSelectRoot variant={variant} key={variant}>
+            <NativeSelectField placeholder={`variant (${variant})`}>
+              <option value="react">React</option>
+              <option value="vue">Vue</option>
+              <option value="angular">Angular</option>
+              <option value="svelte">Svelte</option>
+            </NativeSelectField>
+          </NativeSelectRoot>
+        )}
+      </For>
+      <Input placeholder="Subtle" variant="subtle" className="px-3 rounded-" />
+    </form>
   )
 }
 
