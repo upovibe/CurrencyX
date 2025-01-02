30953bac9bf27f57a54185e0a6d098aacee4f349
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-02 18:16:24 +0000

    refactor: restructure App component and update CSS file paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { Provider } from "@/components/ui/provider";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@/pages/App.tsx";
+import "@/css/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
